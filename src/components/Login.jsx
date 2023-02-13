8012fd4dae5d5303693f9020d17570b54be411e2
Tayamul <rai_tayamul@hotmail.com>
2023-02-13 12:35:49 +0000

    Design a simple login form

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import { Button, Grid, Paper, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+const Login = () => {
+
+    const paperStyle = {width:300, margin:"60px auto", padding:"35px 20px"}
+    const textStyle = {mt:2}
+  return (
+    <Grid>
+        <Paper elevation={10} sx={paperStyle}>
+            <Grid align='center'>
+                <Typography variant='h5'>
+                    Log in
+                </Typography>
+                <Typography variant='caption'>
+                    Don't have an account? Sign up
+                </Typography>
+                <form>
+                    <TextField
+                    label='Email'
+                    id='email'
+                    type='email'
+                    fullWidth
+                    sx={textStyle}
+                    />
+                     <TextField
+                    label='Password'
+                    id='password'
+                    type='password'
+                    fullWidth
+                    sx={textStyle}
+                    />
+                    <Button
+                    type='submit'
+                    variant='contained'
+                    sx={textStyle}
+                    >
+                        Log in
+                    </Button>
+                </form>
+            </Grid>
+        </Paper>
+    </Grid>
+  )
+}
+
+export default Login
