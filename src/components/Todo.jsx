03e17eeaacc2c131677bae63831adce116db21d6
Tayamul <rai_tayamul@hotmail.com>
2023-02-13 13:57:58 +0000

    Import css file into todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './todo.css'
+import {db} from '../firebaseConfig'
+import { collection, getDoc } from 'firebase/firestore'
+import { Button, TextField } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
 
 const Todo = () => {
+
+    // Create data in firebase
+    
+
+
   return (
-    <div>Todo</div>
+    <div className='todo-container'>
+        <form>
+        <TextField id="standard-basic" label="Add a new task" variant="standard" />
+        <Button ><AddIcon/></Button>
+        </form>
+    </div>
   )
 }
 
