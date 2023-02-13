f6a760008035f327e02f7a099d817c55204d62c3
Tayamul <rai_tayamul@hotmail.com>
2023-02-13 00:57:10 +0000

    Create a signup component and imported authentication function from firebase

---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {auth} from '../firebaseConfig'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+const Signup = () => {
+    
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
