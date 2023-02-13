ca345739743fdb9179279820e3c198051628a0cf
Tayamul <rai_tayamul@hotmail.com>
2023-02-13 12:03:12 +0000

    Render Sidebar component globally in the app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { AuthProvider } from "./Auth";
 import { Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import { CssBaseline } from '@mui/material';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <AuthProvider>
     <div className="App">
       <CssBaseline/>
+      <Sidebar/>
       <Routes>
         <Route path='/signup' element={<Signup/>}/>
       </Routes>
