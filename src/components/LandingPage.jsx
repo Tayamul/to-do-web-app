4d01b5978df0c1528ef401cd72614fbe339ae7b0
Tayamul <rai_tayamul@hotmail.com>
2023-02-14 10:27:38 +0000

    Add typography in the landing page explaining a brief intro about the web app

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,17 @@
+import { Paper, Typography } from '@mui/material'
+import { Container } from '@mui/system'
 import React from 'react'
 import './landingPage.css'
 
 const LandingPage = () => {
+    const paperStyle = {padding:"20px 20px", margin:"30px 30px"}
   return (
-    <div>
-
-    </div>
+    <Container fixed maxWidth="sm" sx={{margin:"120px auto"}}>
+        <Paper elevation={0} sx={paperStyle} align='center'>
+            <Typography variant='h6'>A simple web-app allowing you to plan your day according to your needs. Jot down your plans in a list and cross them out once its completed.</Typography>
+            <Typography variant='caption'>Advise: Break down your plans into seprate easy-to-tackle tasks, and then attack it!</Typography>
+        </Paper>
+    </Container>
   )
 }
 
