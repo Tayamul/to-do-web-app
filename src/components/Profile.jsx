3c917f642f52865726daf641e9f8a8980ec017b3
Tayamul <rai_tayamul@hotmail.com>
2023-02-23 11:27:35 +0000

    Design a form-type container for profile section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import {AuthContext} from '../Auth'
+import React, { useContext } from "react";
+import { AuthContext } from "../Auth";
+import "./profile.css";
 
 const Profile = () => {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
-    <div style={{margin:"50px auto", width:"300px"}}>
-       
-    </div>
-  )
-}
+    <section className="profile-container">
+      <img
+        className="profile-image"
+        src={currentUser.photoURL}
+        alt="display avatar"
+      ></img>
+      <div>
+          <p style={{textAlign:"center"}}>{currentUser.displayName}</p>
+        <form className="profile-form">
+          <label>Current Password</label>
+          <input type='password' />
+          <label>New Password</label>
+          <input type='password' />
+          <label>Confirm Password</label>
+          <input type='password' />
+          <button className="profile-btn">Change password</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
