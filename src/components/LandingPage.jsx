0f1ca031a80d03c0b6ceabcd7df296b08aabead6
Tayamul <rai_tayamul@hotmail.com>
2023-03-14 13:54:07 +0000

    Edit description on home page and deployed latest project

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,18 +1,29 @@
-import { Paper, Typography } from '@mui/material'
-import { Container } from '@mui/system'
-import React from 'react'
-import './landingPage.css'
+import { Paper, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import "./landingPage.css";
 
 const LandingPage = () => {
-    const paperStyle = {padding:"20px 20px", margin:"30px 30px"}
+  const paperStyle = { padding: "20px 20px", margin: "30px 30px" };
   return (
-    <Container fixed maxWidth="sm" sx={{margin:"120px auto"}}>
-        <Paper elevation={0} sx={paperStyle} align='center'>
-            <Typography variant='h6'>A simple web-app allowing you to plan your day according to your needs. Jot down your plans in a list and cross them out once its completed.</Typography>
-            <Typography variant='caption'>Advise: Break down your plans into seprate easy-to-tackle tasks, and then attack it!</Typography>
-        </Paper>
+    <Container fixed maxWidth="sm" sx={{ margin: "80px auto" }}>
+      <Paper elevation={0} sx={paperStyle} align="center">
+        <Typography variant="overline">
+          Introducing a user-friendly web application designed to facilitate
+          daily planning in accordance with your personal preferences. With its
+          easy-to-use interface, simply list your tasks and effortlessly mark
+          them as completed upon finishing.
+        </Typography>
+        <br />
+        <Typography variant="caption">
+          For optimal productivity, we recommend dividing larger tasks into
+          smaller, manageable segments, and tackling them one by one. Streamline
+          your day-to-day routine and maximize your efficiency with our
+          innovative planning tool.
+        </Typography>
+      </Paper>
     </Container>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
