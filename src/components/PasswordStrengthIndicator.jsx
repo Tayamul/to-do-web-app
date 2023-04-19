60fd295a30df3689e282fc3b5deba692f23d0d0f
Tayamul <rai_tayamul@hotmail.com>
2023-04-19 17:34:58 +0100

    Add class names where necessary for styling

--- src/components/PasswordStrengthIndicator.jsx
+++ src/components/PasswordStrengthIndicator.jsx
@@ -2,9 +2,9 @@ import './passwordStrengthIndicator.css'
 
 const PasswordStrengthIndicator = ({validate: {minChar, number, specialChar}}) => {
     return (
-        <div>
-            <p>Password must contain:</p>
-            <ul>
+        <div className='password-text'>
+            <p className='text-dark'>Password must contain:</p>
+            <ul className='text-muted'>
                 <PasswordStrengthIndicatorItem isValid={minChar} text='Must be at least 8 characters'/>
                 <PasswordStrengthIndicatorItem isValid={number} text='Must include at least 1 number'/>
                 <PasswordStrengthIndicatorItem isValid={specialChar} text='Must include at least 1 special character'/>
@@ -18,3 +18,5 @@ const PasswordStrengthIndicatorItem = ({isValid, text}) => {
     const highlightClass = isValid ? "text-success" : isValid !== null ? "text-danger" : ""
     return <li className={highlightClass}>{text}</li>
 }
+
+export default PasswordStrengthIndicator;
