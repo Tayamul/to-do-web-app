4950cdc7e857e1eab30443c76509927598d221bb
Tayamul <rai_tayamul@hotmail.com>
2023-04-19 17:31:02 +0100

    Import component and pass down two props

--- src/components/PasswordStrengthIndicator.jsx
+++ src/components/PasswordStrengthIndicator.jsx
@@ -5,14 +5,16 @@ const PasswordStrengthIndicator = ({validate: {minChar, number, specialChar}}) =
         <div>
             <p>Password must contain:</p>
             <ul>
-
+                <PasswordStrengthIndicatorItem isValid={minChar} text='Must be at least 8 characters'/>
+                <PasswordStrengthIndicatorItem isValid={number} text='Must include at least 1 number'/>
+                <PasswordStrengthIndicatorItem isValid={specialChar} text='Must include at least 1 special character'/>
             </ul>
         </div>
     )
 }
 
 
-const PasswordStrengthIndicatorItem = () => {
+const PasswordStrengthIndicatorItem = ({isValid, text}) => {
     const highlightClass = isValid ? "text-success" : isValid !== null ? "text-danger" : ""
     return <li className={highlightClass}>{text}</li>
 }
