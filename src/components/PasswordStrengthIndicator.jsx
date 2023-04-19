409bb7c23ac05fa2574f583d67ff58c6ac198acb
Tayamul <rai_tayamul@hotmail.com>
2023-04-19 17:23:08 +0100

    Create a function to feedback users when signing up

--- src/components/PasswordStrengthIndicator.jsx
+++ src/components/PasswordStrengthIndicator.jsx
@@ -1,1 +1,12 @@
 import './passwordStrengthIndicator.css'
+
+const PasswordStrengthIndicator = ({validate: {minChar, number, specialChar}}) => {
+    return (
+        <div>
+            <p>Password must contain:</p>
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
