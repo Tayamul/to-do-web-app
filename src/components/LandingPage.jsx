4930d38d6c4dd500fb32938de6192775be6745f1
Tayamul <rai_tayamul@hotmail.com>
2023-05-03 22:16:44 +0100

    Create a section element for the home component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,24 +6,29 @@ import "./landingPage.css";
 const LandingPage = () => {
   const paperStyle = { padding: "20px 20px", margin: "30px 30px" };
   return (
-    <Container fixed maxWidth="sm" sx={{ margin: "80px auto" }}>
-      <Paper elevation={0} sx={paperStyle} align="center">
-        <Typography variant="overline">
-          Introducing a user-friendly web application designed to facilitate
-          daily planning in accordance with your personal preferences. With its
-          easy-to-use interface, simply list your tasks and effortlessly mark
-          them as completed upon finishing.
-        </Typography>
-        <br />
-        <Typography variant="caption">
-          For optimal productivity, we recommend dividing larger tasks into
-          smaller, manageable segments, and tackling them one by one. Streamline
-          your day-to-day routine and maximize your efficiency with our
-          innovative planning tool.
-        </Typography>
-      </Paper>
-    </Container>
+    <section id='home'>
+      
+    </section>
   );
 };
 
 export default LandingPage;
+
+
+{/* <Container fixed maxWidth="sm" sx={{ margin: "80px auto" }}>
+<Paper elevation={0} sx={paperStyle} align="center">
+  <Typography variant="overline">
+    Introducing a user-friendly web application designed to facilitate
+    daily planning in accordance with your personal preferences. With its
+    easy-to-use interface, simply list your tasks and effortlessly mark
+    them as completed upon finishing.
+  </Typography>
+  <br />
+  <Typography variant="caption">
+    For optimal productivity, we recommend dividing larger tasks into
+    smaller, manageable segments, and tackling them one by one. Streamline
+    your day-to-day routine and maximize your efficiency with our
+    innovative planning tool.
+  </Typography>
+</Paper>
+</Container> */}
