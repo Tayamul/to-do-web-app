2b817fa9bc43b2032ba2e59dcf1a891209e72ae6
Tayamul <rai_tayamul@hotmail.com>
2023-05-06 11:01:35 +0100

    Import auth function for configuration

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,25 +2,34 @@
 // import { Container } from "@mui/system";
 import React from "react";
 import "./landingPage.css";
-import notes from '../assets/notes.jpg'
+import notes from "../assets/notes.jpg";
 import { Link } from "react-router-dom";
-import { signInAnonymously } from 'firebase/auth'
+import { signInAnonymously } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
 const LandingPage = () => {
   // const paperStyle = { padding: "20px 20px", margin: "30px 30px" };
   return (
-    <section id='home'>
+    <section id="home">
       <div className="home-container">
         <div className="hero-content">
           <h1>Optimise your daily workflow</h1>
-          <p>Improve productivity with our intuitive interface for daily planning</p>
+          <p>
+            Improve productivity with our intuitive interface for daily planning
+          </p>
           <div className="cta-btn">
-          <Link to='/signup'><button className="cta">Sign up</button></Link>
-          <button className="cta-2">Guest login</button>
+            <Link to="/signup">
+              <button className="cta">Sign up</button>
+            </Link>
+            <button className="cta-2">Guest login</button>
           </div>
         </div>
         <div>
-          <img src={notes} alt='An impression of a user writing down tasks' className="hero-image"/>
+          <img
+            src={notes}
+            alt="An impression of a user writing down tasks"
+            className="hero-image"
+          />
         </div>
       </div>
     </section>
@@ -29,8 +38,8 @@ const LandingPage = () => {
 
 export default LandingPage;
 
-
-{/* <Container fixed maxWidth="sm" sx={{ margin: "80px auto" }}>
+{
+  /* <Container fixed maxWidth="sm" sx={{ margin: "80px auto" }}>
 <Paper elevation={0} sx={paperStyle} align="center">
   <Typography variant="overline">
     Introducing a user-friendly web application designed to facilitate
@@ -46,4 +55,5 @@ export default LandingPage;
     innovative planning tool.
   </Typography>
 </Paper>
-</Container> */}
+</Container> */
+}
